f2fd24772bf316465836d2e8f7194d0531921cb8
ahmad shakik <ahmadshakik777@gmail.com>
2024-01-03 01:52:22 +0200

    added

--- lib/mongodb/database/index.ts
+++ lib/mongodb/database/index.ts
@@ -1,16 +1,21 @@
 import mongoose from "mongoose";
 
-const MONOGO_URI = process.env.MONGODB_URI;
-console.log(MONOGO_URI);
+const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  if (!MONOGO_URI) throw new Error("MongoDB uri is missing");
+  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
 
-  cached.promise = mongoose.connect(MONOGO_URI);
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: "evently",
+      bufferCommands: false,
+    });
+
   cached.conn = await cached.promise;
 
   return cached.conn;
